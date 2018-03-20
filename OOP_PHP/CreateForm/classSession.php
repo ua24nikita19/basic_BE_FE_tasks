b7095d0de1267c2ddb9f995934ae9ffcd342ae73
Nikita <ua24nikita19@gmail.com>
2018-03-20 23:02:19 +0200

    Add new class <classFlash>

--- OOP_PHP/CreateForm/classSession.php
+++ OOP_PHP/CreateForm/classSession.php
@@ -1,55 +1,54 @@
 <?php
 
-
 class classSession
 {
-
     public static $started = false;
 
     public function __construct($runSession = false)
     {
-        if($runSession === true){
+        if ($runSession === true) {
             $this->startSession();
             self::$started = true;
         }
     }
 
-    protected function startSession(){
+    protected function startSession()
+    {
         session_start();
     }
 
-    protected function stopSession(){
+    protected function stopSession()
+    {
 //        if(session_id()!=""){
 //            session_destroy();
 //        }
-        if(self::$started==true){//ЕСЛИ МЫ СОЗДАДИМ НОВОЫЙ ОБЪЕКТ КЛАССА, ТО СТАТИК $started будет false для каждого нового?
+        if (self::$started == true) {//ЕСЛИ МЫ СОЗДАДИМ НОВОЫЙ ОБЪЕКТ КЛАССА, ТО СТАТИК $started будет false для каждого нового?
             session_unset();
         }
     }
 
-    public function getSession($name)
+    public static function getSession($name)
     {
-        if($_SESSION[$name]){
+        if ($_SESSION[$name]) {
             return $_SESSION[$name];
         }
         return null;
     }
 
-    public function setSession($name,$value): void
+    public static function setSession($name, $value): void
     {
-        if(!$_SESSION[$name]){
+        if (!isset($_SESSION[$name])) {
             $_SESSION[$name] = $value;
         }
     }
 
     public function __isset($name)
     {
-        if(isset($name)){
+        if (isset($name)) {
             return $name;
         }
         return null;
     }
-
 }
 
 
--- OOP_PHP/CreateForm/index.php
+++ OOP_PHP/CreateForm/index.php
@@ -1,19 +1,26 @@
 <?php
 
-require ('classSession.php');
-require ('classSmartForm.php');
-require ('classCookie.php');
+require('classSession.php');
+require('classSmartForm.php');
+require('classCookie.php');
+require('classFlash.php');
 
+//Создаем сессию (класс сессий)
 $session = new classSession(true);
-$session->setSession('userIP',$_SERVER['HTTP_USER_AGENT']);
+$session->setSession('userIP', $_SERVER['HTTP_USER_AGENT']);
 $echo = $session->getSession('userIP');
 
-if($cookie->getCookie('countVisits') != null){
-    Cookie::$counter=$_COOKIE['countVisits']+1;
-}else{
+$flash = new classFlash;
+$flash->setMessage('Success');
+$echo1 = $flash->getMessage();
+
+//Записываем куку-счетчик обновления страницы (класс куки)
+if ($cookie->getCookie('countVisits') != null) {
+    Cookie::$counter = $_COOKIE['countVisits'] + 1;
+} else {
     Cookie::$counter++;
 }
-$cookie->setCookie('countVisits',Cookie::$counter,10);
+$cookie->setCookie('countVisits', Cookie::$counter, 10);
 
 ?>
 
@@ -23,7 +30,7 @@ $cookie->setCookie('countVisits',Cookie::$counter,10);
     <meta charset="UTF-8">
     <title>Title</title>
     <style>
-        .form{
+        .form {
             border: 1px solid black;
             width: 200px;
             height: auto;
@@ -34,14 +41,14 @@ $cookie->setCookie('countVisits',Cookie::$counter,10);
 </head>
 <body>
 <?php
-echo $form->openForm(['class'=>'form','action'=>'index.php','method'=>'POST']);
-echo $form->createElement('input',['type'=>'text','placeholder'=>'name','name'=>'username','value'=>$smartForm->saveName()]);
-echo $form->createElement('input',['type'=>'password','name'=>'userpass','placeholder'=>'password']);
-echo $form->createElement('textarea',['cols'=>19,'rows'=>5,'name'=>'text']).$smartForm->saveText().'</textarea><br>';
-echo $form->createElement('input',['type'=>'submit','value'=>'Post','name'=>'send']);
+echo $form->openForm(['class' => 'form', 'action' => 'index.php', 'method' => 'POST']);
+echo $form->createElement('input', ['type' => 'text', 'placeholder' => 'name', 'name' => 'username', 'value' => $smartForm->saveName()]);
+echo $form->createElement('input', ['type' => 'password', 'name' => 'userpass', 'placeholder' => 'password']);
+echo $form->createElement('textarea', ['cols' => 19, 'rows' => 5, 'name' => 'text']) . $smartForm->saveText() . '</textarea><br>';
+echo $form->createElement('input', ['type' => 'submit', 'value' => 'Post', 'name' => 'send']);
 echo $form->closeForm();
-echo '<br>ECho:'.$echo;
+echo '<br>Echo:' . $echo . '<br>' . 'Message from Flash class:<b>' . $echo1 . '</b>';
 ?>
-<p>Счетчик посещений:<?php echo $cookie->getCookie('countVisits')+1; ?></p>
+<p>Счетчик посещений:<?php echo $cookie->getCookie('countVisits') + 1; ?></p>
 </body>
 </html>
