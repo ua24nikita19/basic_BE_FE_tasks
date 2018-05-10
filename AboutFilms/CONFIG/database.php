3b6de68357858d275a2f8ad9308e724e1c3127f0
olkani <ua24nikita19@gmail.com>
2018-05-10 11:55:00 +0300

    Refactor

---
+++ AboutFilms/CONFIG/database.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'host'=>'localhost',
+    'user'=>'root',
+    'password'=>'щдлфтш95',
+    'db_name'=>'FilmsActors'
+];
---
+++ AboutFilms/VIEWS/import.php
@@ -0,0 +1,19 @@
+<?php
+require './includes/import_inc.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Cache-Control" content="no-store" />
+    <title>TEST</title>
+    <link rel="stylesheet" href="../CSS/stylesImport.css">
+</head>
+<body>
+<form action="" method="post" enctype="multipart/form-data">
+    <input type="file" name="imgfile">
+    <button type="submit">Import</button>
+</form>
+</body>
+</html>
